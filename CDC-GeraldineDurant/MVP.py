dd78371332513e612c6bbd7903238383923abdaa
Chicagolil <147921049+Chicagolil@users.noreply.github.com>
2023-12-16 22:42:39 +0100

    Add files via upload

---
+++ CDC-GeraldineDurant/MVP.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def dashboard():
+    # Exemple de sites à surveiller
+    sites = ["http://example.com", "http://example.org"]
+    status = {}
+
+    for site in sites:
+        try:
+            response = requests.get(site)
+            status[site] = "En ligne" if response.status_code == 200 else "Hors ligne"
+        except requests.ConnectionError:
+            status[site] = "Hors ligne"
+
+    return render_template('dashboard.html', status=status)
+
+if __name__ == '__main__':
+    app.run(debug=True)
